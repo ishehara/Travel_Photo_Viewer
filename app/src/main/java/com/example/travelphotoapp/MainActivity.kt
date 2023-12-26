4365bcd52670a1a331c1f2174b4613b6f7edc805
Ishan Shehara <mlisliyanage@gmail.com>
2023-12-26 22:43:28 +0530

    Update MainActivity.kt
    adding a function to change the text view box

--- app/src/main/java/com/example/travelphotoapp/MainActivity.kt
+++ app/src/main/java/com/example/travelphotoapp/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     var currentImage = 0
@@ -15,6 +16,7 @@ class MainActivity : AppCompatActivity() {
 
         val next = findViewById<ImageButton>(R.id.btnNext)
         val prev= findViewById<ImageButton>(R.id.btnPrevious)
+        var imageText = findViewById<TextView>(R.id.nameOfPlace)
 
         next.setOnClickListener{
             //getting the next image
@@ -29,6 +31,16 @@ class MainActivity : AppCompatActivity() {
             var idImageToShowInt = this.resources.getIdentifier(idImageToShowString, "id", packageName)
             image = findViewById(idImageToShowInt)
             image.alpha = 1f
+
+
+            when(currentImage){
+                0->imageText.text="Coconut Tree Hills"
+                1->imageText.text="Galle"
+                2->imageText.text="Nelligala"
+                3->imageText.text="Red Mosque"
+                4->imageText.text="Ruwanweli Seya"
+                5->imageText.text="Sigiriya"
+            }
         }
 
         prev.setOnClickListener{
@@ -43,6 +55,16 @@ class MainActivity : AppCompatActivity() {
             var idImageToShowInt = this.resources.getIdentifier(idImageToShowString, "id", packageName)
             image = findViewById(idImageToShowInt)
             image.alpha = 1f
+
+
+            when(currentImage){
+                0->imageText.text="Coconut Tree Hills"
+                1->imageText.text="Galle"
+                2->imageText.text="Nelligala"
+                3->imageText.text="Red Mosque"
+                4->imageText.text="Ruwanweli Seya"
+                5->imageText.text="Sigiriya"
+            }
         }
 
     }

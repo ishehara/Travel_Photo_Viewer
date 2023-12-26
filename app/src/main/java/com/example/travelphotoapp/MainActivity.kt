9902325d2fd4c472e1cc86d845f0da92b734f2b2
Ishan Shehara <mlisliyanage@gmail.com>
2023-12-26 22:50:44 +0530

    different way
    create a function to show the name of the place

--- app/src/main/java/com/example/travelphotoapp/MainActivity.kt
+++ app/src/main/java/com/example/travelphotoapp/MainActivity.kt
@@ -2,19 +2,27 @@ package com.example.travelphotoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     var currentImage = 0
     lateinit var image:ImageView
+    lateinit var imageText:TextView
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //make the connection with button on the frontend
         val next = findViewById<ImageButton>(R.id.btnNext)
         val prev= findViewById<ImageButton>(R.id.btnPrevious)
+
 
         next.setOnClickListener{
             //getting the next image
@@ -29,6 +37,11 @@ class MainActivity : AppCompatActivity() {
             var idImageToShowInt = this.resources.getIdentifier(idImageToShowString, "id", packageName)
             image = findViewById(idImageToShowInt)
             image.alpha = 1f
+
+
+            showName(currentImage)
+
+
         }
 
         prev.setOnClickListener{
@@ -43,7 +56,31 @@ class MainActivity : AppCompatActivity() {
             var idImageToShowInt = this.resources.getIdentifier(idImageToShowString, "id", packageName)
             image = findViewById(idImageToShowInt)
             image.alpha = 1f
+
+
+            showName(currentImage)
+
+
         }
 
+
     }
+     //creating a function to show Name of the place in the textView box
+    fun showName(num:Int){
+         imageText = findViewById<TextView>(R.id.nameOfPlace)
+
+        when(num){
+            0->imageText.text="Coconut Tree Hills"
+            1->imageText.text="Galle"
+            2->imageText.text="Nelligala"
+            3->imageText.text="Red Mosque"
+            4->imageText.text="Ruwanweli Seya"
+            5->imageText.text="Sigiriya"
+        }
+    }
+
+
+
+
 }
+
